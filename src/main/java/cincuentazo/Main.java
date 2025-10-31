fd6b7b0cb4afb1bdf815cc836f7637a16b56f39c
Dana Sofia Gomez Manrique <danagomez2816@gmail.com>
2025-10-31 10:43:16 -0500

    Se cambió la clase Main que para iniciar la aplicación y mostrar la ventana de inicio

--- src/main/java/cincuentazo/Main.java
+++ src/main/java/cincuentazo/Main.java
@@ -1,8 +1,7 @@
 package cincuentazo;
 
+import cincuentazo.views.InicioView;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +9,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("inicio-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        InicioView inicioView = InicioView.getInstance();
+        inicioView.show();
     }
 }
