c487e625d53a09b98d5bc017917c76b05bd6786d
Laura Valentina Bernal Lozada <laurabernal1912@gmail.com>
2025-10-31 13:17:59 -0500

    importe innecesario

--- src/main/java/cincuentazo/controllers/VentanaGanadorController.java
+++ src/main/java/cincuentazo/controllers/VentanaGanadorController.java
@@ -1,7 +1,6 @@
 package cincuentazo.controllers;
 
 import cincuentazo.views.InicioView;
-import cincuentazo.views.JuegoView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
