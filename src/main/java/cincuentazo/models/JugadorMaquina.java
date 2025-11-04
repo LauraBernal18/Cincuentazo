64a1f440624d7aa9c56cf6c550dc84e7d4fa782d
Dana Sofia Gomez Manrique <danagomez2816@gmail.com>
2025-11-03 21:16:05 -0500

    Borarador de la clase jugador el cual hereda a jugador humano y jugador maquina

--- src/main/java/cincuentazo/models/JugadorMaquina.java
+++ src/main/java/cincuentazo/models/JugadorMaquina.java
@@ -1,4 +1,47 @@
 package cincuentazo.models;
 
-public class JugadorMaquina {
+public class JugadorMaquina extends Jugador {
+
+    private static int contador = 1;
+
+    public JugadorMaquina(String nombre) {
+        super("Máquina " + contador);
+        contador++;
+    }
+
+    /*public void esperarJugador() {
+        try {
+            int tiempo = (int) (Math.random() * 2000) + 2000; // 2000 - 4000 ms
+            Thread.sleep(tiempo);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }*/
+
+
+    @Override
+    public Carta seleccionarCarta(int sumaMesa) {
+
+        //esperarJugador();
+
+        // Recorremos todas las cartas
+        /*for (int i = 0; i < mano.size(); i++) {
+
+            Carta carta = mano.get(i);
+
+            // Calculamos la suma si se jugara esta carta
+            int nuevaSuma = sumaMesa + carta.getValorSegunReglas(sumaMesa);
+
+            // Si no se pasa de 50 la carta sirve
+            if (nuevaSuma <= 50) {
+                return carta; // devolvemos directamente la carta
+            }
+        }*/
+
+            // Si el ciclo termina y no se encontró ninguna carta válida
+            this.eliminado = true;
+            return null;
+
+        }
+
 }
