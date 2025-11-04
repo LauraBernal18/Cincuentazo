6a9f7a0655ad1334ed0d26bc7698e8c7fefe8471
Laura Valentina Bernal Lozada <laurabernal1912@gmail.com>
2025-11-04 10:44:51 -0500

    enlazar cada carta a una imagen

--- src/main/java/cincuentazo/models/Carta.java
+++ src/main/java/cincuentazo/models/Carta.java
@@ -1,12 +1,30 @@
 package cincuentazo.models;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Carta {
     private String valor;  // "A", "2", "3", ..., "K"
     private String palo;   // "bombón", "burbuja", "bellota", "falta este"
+    private ImageView imagen; //imagen asociada a cada carta
 
     public Carta(String valor, String palo) {
         this.valor = valor;
         this.palo = palo;
+        this.imagen = crearImagenCarta();
+    }
+
+    private ImageView crearImagenCarta() {
+        String ruta = "/cincuentazo/images/cartas/" + valor + "_de_" + palo + ".png";
+        ImageView imagen = new ImageView(new Image(getClass().getResourceAsStream(ruta)));
+        imagen.setFitWidth(90);
+        imagen.setFitHeight(120);
+        return imagen;
+    }
+
+    //la usamos luego en el controlador para obtener la imagen de cada carta en el fxml y asociarle el evento de mouseclicked
+    public ImageView getImagen() {
+        return imagen;
     }
 
     public String getValor() {
